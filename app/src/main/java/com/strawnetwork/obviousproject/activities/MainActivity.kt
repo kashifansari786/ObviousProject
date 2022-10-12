ffd2e7be5495d95dd797633242e2ac3602e044f8
kashif ansari <mhd.kashif1@gmail.com>
2022-10-12 20:27:37 +0530

    adding recyclerview and adapter for population data

--- app/src/main/java/com/strawnetwork/obviousproject/activities/MainActivity.kt
+++ app/src/main/java/com/strawnetwork/obviousproject/activities/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.strawnetwork.obviousproject.activities
 
 
-import android.arch.lifecycle.ViewModelProviders
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.strawnetwork.obviousproject.R
+import com.strawnetwork.obviousproject.adapters.NasaImagesAdapter
 import com.strawnetwork.obviousproject.viewModels.MainActivityViewModel
+import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +26,28 @@ class MainActivity : AppCompatActivity() {
         mMainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         //pass refrence of activity to viewmodel
         mMainActivityViewModel!!.NasaImagesData(this@MainActivity)
-
+       // setUpNavigation()
+        observeData()
     }
-
+    private fun observeData() {
+        //observe data from viewmodel when live data have data(means when livedata post the value)
+        mMainActivityViewModel!!.getNasaData?.observe(this) { nicePlaces ->
+            recyclerView.apply {
+                setHasFixedSize(true)
+                //set grid layout manager to recyclerview with count 2
+                layoutManager= GridLayoutManager(this@MainActivity,2)
+                //add decoration to recyclerview horizxonral line between 2 cards
+                addItemDecoration(
+                    DividerItemDecoration(recyclerView.context,
+                        DividerItemDecoration.HORIZONTAL)
+                )
+                addItemDecoration(
+                    DividerItemDecoration(recyclerView.context,
+                        DividerItemDecoration.VERTICAL)
+                )
+                //initilize the adapter with arraylist of data and set to recyclerview
+                adapter= NasaImagesAdapter(this@MainActivity,nicePlaces)
+            }
+        }
+    }
 }
